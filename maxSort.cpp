d2d8c3a6d1055252858161afdbc288932b880fef
conma <milecw17@wfu.edu>
2019-09-12 15:46:39 -0400

    final

--- maxSort.cpp
+++ maxSort.cpp
@@ -9,22 +9,22 @@ void print(const vector<int>& list);
 
 int main() {
 
-    vector<int> v(5);  // vector to be sorted
+    vector<int> v (5);  // vector to be sorted
     // initialize v
-    /* your code here */
+    v={6,1,3,2,1};
 
-
-    // print the unsorted v
-    /* your code here */
+// print the unsorted v
+    cout<<"unsorted: "<<endl;
+    print(v);
+    cout<<endl;
 
 
     // sort v
-    /* your code here */
-
+    maxSort(v);
 
     // print the sorted v
-    /* your code here */
-
+    cout<<"Sorted: "<<endl;
+    print(v);
 
     return 0;
 }
@@ -34,23 +34,31 @@ int main() {
 //Max sort function
 void maxSort(vector<int>& list){
     for(int i=list.size()-1;i>=0;i--){
-        /*complete the code*/
-
+        int currMax = 0;
         for(int j=0;j<=i;j++){
-            /*complete the code*/
+            if (list.at(j)>=list.at(currMax)) {
+                currMax =j;
+            }
+            swap(list.at(currMax), list.at(i));
 
         }
-        /*complete the code*/
     }
 }
 
 // swap function
 void swap(int& a, int&b){
-    /*your code here*/
+
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
 }
 
 // print function
 void print(const vector<int>& list){
-    /*your code here*/
-}
+    for (int i=0;i<list.size();i++) {
 
+        cout<< list.at(i)<< " ";
+    }
+
+}
